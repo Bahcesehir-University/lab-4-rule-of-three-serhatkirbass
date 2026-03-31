2023b7aa1766f9f10d8bda55fe295ffbfb03a65d
serhatkirbass <s_kirb@hotmail.com>
2026-03-31 14:48:27 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -116,7 +116,10 @@ public:
         //    1. Increment g_assignmentCount
         g_assignmentCount++;
         //    2. Check for self-assignment (if this == &other)
-        if (this == &other) return *this;
+        if (this == &other)
+    
+        {return *this;
+        }
             //    3. Delete old m_data (delete[])
             delete[] m_data;
             //    4. Copy m_length from other
@@ -125,7 +128,7 @@ public:
             m_data = new char[m_length + 1];
             //    6. Copy string data using strcpy()
             strcpy(m_data, other.m_data);
-        }
+        
         //    7. Return *this
         return *this;
     }
